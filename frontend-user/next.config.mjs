0f9447343cf3b505bbadf9c9b52bb2906ca99df0
duong <22520298@gm.uit.edu.vn>
2026-01-21 00:09:25 +0700

    Update final

--- frontend-user/next.config.mjs
+++ frontend-user/next.config.mjs
@@ -7,7 +7,6 @@ const __dirname = path.dirname(__filename);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   outputFileTracingRoot: __dirname,
-  webpack: (config) => config,
 };
 
 export default nextConfig;
