24f705eb1373bf0266569bab4397fd8021f3eb27
duong <22520298@gm.uit.edu.vn>
2026-01-21 00:14:28 +0700

    Update turpo

--- frontend-user/next.config.mjs
+++ frontend-user/next.config.mjs
@@ -6,7 +6,12 @@ const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: false,           
   outputFileTracingRoot: __dirname,
+
+  webpack(config) {
+    return config;
+  },
 };
 
 export default nextConfig;
